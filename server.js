18ef0db528693567fb5c26d60db0707583b7cf22
Karthik Ramachandran <karthik@Karthiks-MacBook-Air.local>
2018-12-19 22:00:30 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const expressMod = require('express');
+const app = expressMod();
+
+app.get('/', (req, res) => {
+  res.send('Hello Oruvan...!!');
+});
+
+app.listen(8080, () => {
+  console.log('Server listening on port 8080!');
+});
